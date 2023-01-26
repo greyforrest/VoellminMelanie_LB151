09268ded34ab97eccc83a431771b2be127a60e26
greyforrest <melanie.voellmin@gmail.com>
2023-01-26 17:33:27 +0100

    First ugly not really working version

---
+++ Glücksrad/Data/FirebaseService.cs
@@ -0,0 +1,9 @@
+﻿using Firebase.Database;
+
+namespace Glücksrad.Data
+{
+    public class FirebaseService
+    {
+        FirebaseClient firebase = new FirebaseClient("https://gluecksspiel-8822f-default-rtdb.europe-west1.firebasedatabase.app/");
+    }
+}
